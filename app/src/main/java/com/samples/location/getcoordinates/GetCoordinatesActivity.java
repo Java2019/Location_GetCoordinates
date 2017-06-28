7d4b19c585997cf28c88136dfaa26a03bf1bbe2d
java2019 <java_2019@mail.ru>
2017-06-28 13:16:11 +0300

    test1

--- app/src/main/java/com/samples/location/getcoordinates/GetCoordinatesActivity.java
+++ app/src/main/java/com/samples/location/getcoordinates/GetCoordinatesActivity.java
@@ -1,13 +1,53 @@
 package com.samples.location.getcoordinates;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GetCoordinatesActivity extends AppCompatActivity {
+
+    private LocationManager manager;
+    private TextView text;
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            printLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+            printLocation(null);
+        }
+    };
+
+    private void printLocation(Object o) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_coordinates);
+
+        text = (TextView)findViewById(R.id.text);
+        manager = (LocationManager)getSystemService(LOCATION_SERVICE);
+
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+
     }
 }
